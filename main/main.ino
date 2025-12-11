c234554533bd14e55bc8f388066a31f56a92db9c
ArtMeTT1 <craveiroarthur@gmail.com>
2025-12-11 14:26:47 -0300

    added i2c communication to main(esp32)

--- main/main.ino
+++ main/main.ino
@@ -6,6 +6,28 @@ ESP 32
 /* PORTS */
 
 
+/*CONFIGURACAO: COMUNICACAO I2C*/
+
+/*Envia o comando para o dispositivo secundario, solicitando o dado */
+void enviarI2C(int comando) {
+  Wire.beginTransmission(SLAVE_ADDR);
+  Wire.write(comando);
+  Wire.endTransmission();
+}
+
+/* Recebe o dado do dispositivo secundario, como float*/
+float receberFloat() {
+  float valor = 0.0;
+
+  Wire.requestFrom(SLAVE_ADDR, sizeof(float));
+  byte *p = (byte*)&valor;
+  int i = 0;
+
+  while (Wire.available() && i < sizeof(float)) {
+    p[i++] = Wire.read();
+  }
+  return valor;
+}
 
 void setup() {
   // put your setup code here, to run once:
